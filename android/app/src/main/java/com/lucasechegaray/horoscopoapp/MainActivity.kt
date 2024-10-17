6fd1ad2b44e52d5803c9a5a3c221ecc05239e935
Lucas-aquiles <lucasechegaray2011@gmail.com>
2024-10-17 01:12:09 -0300

    feat: changes in bundle identifiers

--- android/app/src/main/java/com/lucasechegaray/horoscopoapp/MainActivity.kt
+++ android/app/src/main/java/com/lucasechegaray/horoscopoapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.horoscopo
+package com.lucasechegaray.horoscopoapp
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/lucasechegaray/horoscopoapp/MainApplication.kt
+++ android/app/src/main/java/com/lucasechegaray/horoscopoapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.horoscopo
+package com.lucasechegaray.horoscopoapp
 
 import android.app.Application
 import com.facebook.react.PackageList
